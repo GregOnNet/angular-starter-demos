8e242e6357389a379b05f33ff8de4a850bce71a7
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-03-05 13:02:12 +0100

    Injecting bower-files and own js-components

--- automation/gulpfile.js
+++ automation/gulpfile.js
@@ -1,14 +1,17 @@
 'use strict';
 
 var gulp   = require('gulp'),
-    inject = require('gulp-inject');
+    inject = require('gulp-inject'),
+    bower  = require('main-bower-files');
 
 gulp.task('index', function() {
 
-  var target  = gulp.src('./src/index.html'),
-      sources = gulp.src(['./src/components/**/*.js'], {read: false});
+  var target      = gulp.src('./src/index.html'),
+      bowerStream = gulp.src(bower(), {read:false}),
+      appStream   = gulp.src(['./src/components/**/*.js'], {read: false});
 
   target
-    .pipe(inject(sources, {relative: true}))
+    .pipe(inject(bowerStream, {name: 'bower', relative: true}))
+    .pipe(inject(appStream,   {name: 'app',   relative: true}))
     .pipe(gulp.dest('./src'));
 });
