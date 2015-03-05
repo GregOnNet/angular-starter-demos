b58398591a5ec3212e84fe6db2e5d0c175f90837
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-03-05 08:39:14 +0100

    Adding task minifying images

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,11 +1,24 @@
 'use strict';
 
-var gulp   = require('gulp'),
-    jshint = require('gulp-jshint');
+var gulp     = require('gulp'),
+    jshint   = require('gulp-jshint'),
+    changed  = require('gulp-changed'),
+    imagemin = require('gulp-imagemin');
 
 // JS hint task
 gulp.task('jshint', function() {
   gulp.src('./src/scripts/*.js')
       .pipe(jshint())
       .pipe(jshint.reporter('default'));
+})
+
+// Minify new images
+gulp.task('imagemin', function() {
+  var source      = './src/images/**/*',
+      destination = './build/images';
+
+  gulp.src(source)
+      .pipe(changed(destination))
+      .pipe(imagemin())
+      .pipe(gulp.dest(destination));
 })
