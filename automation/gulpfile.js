f5d4336bb988132738cf8a61f0aaf26c1ea86353
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-03-05 16:45:33 +0100

    Configuring browser-sync

--- automation/gulpfile.js
+++ automation/gulpfile.js
@@ -1,21 +1,44 @@
 'use strict';
 
-var gulp   = require('gulp'),
-    inject = require('gulp-inject'),
-    bower  = require('main-bower-files');
+var gulp            = require('gulp'),
+    browserSync     = require('browser-sync'),
+    inject          = require('gulp-inject'),
+    bower           = require('main-bower-files'),
+    angularFileSort = require('gulp-angular-filesort');
 
-var angularFileSort = require('gulp-angular-filesort');
+var paths = {
+  src : './src/'
+};
 
-var srcDir = './src/';
+var appFiles = [
+  paths.src + 'components/**/*.js',
+  paths.src + '/*.html'
+];
 
 gulp.task('inject', function() {
 
-  var target      = gulp.src(srcDir + 'index.html'),
-      appStream   = gulp.src(srcDir + 'components/**/*.js').pipe(angularFileSort()),
+  var target      = gulp.src(paths.src + 'index.html'),
+      appStream   = gulp.src(paths.src + 'components/**/*.js').pipe(angularFileSort()),
       bowerStream = gulp.src(bower(), {read:false});
 
   target
     .pipe(inject(appStream,   {name: 'app',   relative: true}))
     .pipe(inject(bowerStream, {name: 'bower', relative: true}))
-    .pipe(gulp.dest(srcDir));
+    .pipe(gulp.dest(paths.src));
+});
+
+gulp.task('serve', ['watch'], function(){
+  browserSync(
+    appFiles,
+    { startPath: paths.src,
+      server: {
+        baseDir: "./" }
+    }
+  );
+});
+
+gulp.task('watch', function(){
+  gulp.watch([
+    'bower.json'
+  ], ['inject']);
 });
